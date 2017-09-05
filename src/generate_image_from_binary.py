59c3d2a58be2fb538ce4473319778d41e495b026
Jia, Yongheng <jiayongheng20@gmail.com>
2017-09-05 12:32:22 +0800

    modified on ubuntu, generate images from particular files in a directory.

--- src/generate_image_from_binary.py
+++ src/generate_image_from_binary.py
@@ -6,7 +6,7 @@ import binascii
 from os import listdir
 from os.path import isfile, join
 
-def getMatrixfrom_bin(filename,  width):
+def getMatrixfrom_bin(filename, width):
     with open(filename, 'rb') as f:
         content = f.read()
     hexst = binascii.hexlify(content)  #将二进制文件转换为十六进制字符串
@@ -16,10 +16,12 @@ def getMatrixfrom_bin(filename,  width):
     fh = numpy.uint8(fh)
     return fh
 
-path = "C:\Users\Nicola\PycharmProjects\play-field\data"
-files = [f for f in listdir(path) if isfile(join(path, f))]
+source_path = "/home/nick/research/vs/Virus.Win"
+all_files = [f for f in listdir(source_path) if isfile(join(source_path, f))]
+key_word = "Virus.Win32.Bube"
+particular_files = [f for f in all_files if key_word in f]
 
-for f in files:
-    filename = join(path, f)
-    im = Image.fromarray(getMatrixfrom_bin(filename, 512)) #转换为图像
-    im.save("../output/{0}.png".format(f))
+for f in particular_files:
+    filename = join(source_path, f)
+    im = Image.fromarray(getMatrixfrom_bin(filename, 64)) #转换为图像
+    im.save("/home/nick/research/vs-output-images/Virus.Win.Output.Images/{0}.png".format(f))
